0e95b8ee05b247a7ea0aa8a61d4078e9b136f0fb
Ezequiel Becerra <ezequiel.becerra@gmail.com>
2017-08-26 14:10:10 -0300

    tweets 3 results instead of 1 + longer titles (100 char)

--- app.rb
+++ app.rb
@@ -12,16 +12,15 @@ replies do |tweet|
     uri = URI(URI.escape("http://api.canillitapp.com/search/#{search}"))
     response = Net::HTTP.get(uri)
 
-    json = JSON.parse(response)
-    news = json.first
+    json = JSON.parse(response).take(3).each do |news|
+      src = "#USER# No encontré resultados con #{search} ^B"
+      unless news.nil?
+        title = news['title'].length > 100 ? "#{news['title'][0, 100]} (..)" : news['title']
+        src = "#USER# '#{title}' #{news['url']} ^B"
+      end
 
-    src = "#USER# No encontré resultados con #{search} ^B"
-    unless news.nil?
-      title = news['title'].length > 60 ? "#{news['title'][0, 60]} (..)" : news['title']
-      src = "#USER# '#{title}' #{news['url']} ^B"
+      reply src, tweet
     end
-
-    reply src, tweet
   rescue => e
     puts e
   end
